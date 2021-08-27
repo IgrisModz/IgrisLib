8f3229c1e4caf1495502e7f748cca7d6e5dccf93
IgrisModz <48447742+IgrisModz@users.noreply.github.com>
2021-08-27 18:15:55 +0200

    Added for native extension functions.

---
+++ IgrisLib.MahApps.Metro/Extra/TypeCache.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace IgrisLib
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public static class TypeCache<T>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static int Size { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static Type Type { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static TypeCode TypeCode { get; }
+
+        static TypeCache()
+        {
+            Type = typeof(T);
+
+            if (Type.IsEnum)
+            {
+                Type = Type.GetEnumUnderlyingType();
+            }
+
+            Size = Type == typeof(IntPtr) ? IntPtr.Size : Type == typeof(bool) ? 1 : Marshal.SizeOf<T>();
+
+            TypeCode = Type.GetTypeCode(Type);
+        }
+    }
+}
---
+++ IgrisLib/Extra/TypeCache.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace IgrisLib
+{
+    public static class TypeCache<T>
+    {
+        public static readonly int Size;
+        public static readonly Type Type;
+        public static readonly TypeCode TypeCode;
+
+        static TypeCache()
+        {
+            Type = typeof(T);
+
+            if (Type.IsEnum)
+            {
+                Type = Type.GetEnumUnderlyingType();
+            }
+
+            Size = Type == typeof(IntPtr) ? IntPtr.Size : Type == typeof(bool) ? 1 : Marshal.SizeOf<T>();
+
+            TypeCode = Type.GetTypeCode(Type);
+        }
+    }
+}
