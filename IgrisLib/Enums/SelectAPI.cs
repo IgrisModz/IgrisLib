6dec793be47c1adbcaac5f76c2e80c8c748676b3
IgrisModz <48447742+IgrisModz@users.noreply.github.com>
2021-08-27 18:22:31 +0200

    Add commentary

--- IgrisLib/Enums/SelectAPI.cs
+++ IgrisLib/Enums/SelectAPI.cs
@@ -1,10 +1,25 @@
 ﻿namespace IgrisLib
 {
+    /// <summary>
+    /// Enumerable for choose your api
+    /// </summary>
     public enum SelectAPI
     {
+        /// <summary>
+        /// No API select 
+        /// </summary>
         Null,
+        /// <summary>
+        /// Api control console work for DEX and CEX
+        /// </summary>
         CCAPI,
+        /// <summary>
+        /// Api PS3 Manager work for DEX, CEX and HEN
+        /// </summary>
         PS3MAPI,
+        /// <summary>
+        /// Api Target Manager work only for DEX
+        /// </summary>
         TMAPI
     }
 }
