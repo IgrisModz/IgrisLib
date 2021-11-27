cd0753afbbfaea52163afcbff38be951a6296f4a
IgrisModz <48447742+IgrisModz@users.noreply.github.com>
2021-11-27 13:17:44 +0100

    Add Apis list

---
+++ IgrisLib/Helpers/ApiHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IgrisLib.Helpers
+{
+    internal static class ApiHelper
+    {
+        private static IEnumerable<Type> apis;
+
+        internal static IEnumerable<Type> Apis
+        {
+            get
+            {
+                return apis = apis ?? AppDomain.CurrentDomain.GetAssemblies()
+                        .SelectMany(x => x.GetTypes())
+                        .Where(x => typeof(IApi).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract);
+            }
+        }
+    }
+}
