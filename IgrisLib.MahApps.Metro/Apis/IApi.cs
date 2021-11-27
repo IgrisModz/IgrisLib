07f23396dde80fece5b9301c8f935c7f5d2a5652
IgrisModz <48447742+IgrisModz@users.noreply.github.com>
2021-11-27 13:24:31 +0100

    Add commentaries

---
+++ IgrisLib.MahApps.Metro/Apis/IApi.cs
@@ -0,0 +1,68 @@
+﻿namespace IgrisLib
+{
+    /// <summary>
+    /// Interface for all api in this library.
+    /// </summary>
+    public interface IApi
+    {
+        /// <summary>
+        /// Full name of this api.
+        /// </summary>
+        string FullName { get; }
+
+        /// <summary>
+        /// Name of this api.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// IP address currently in use.
+        /// </summary>
+        string IPAddress { get; }
+
+        /// <summary>
+        /// Connect your tool to your ps3.
+        /// </summary>
+        /// <returns>Returns <see cref="bool.TrueString"/> if you're successufy connected.</returns>
+        bool ConnectTarget();
+
+        /// <summary>
+        /// Attach your tool to your current game.
+        /// </summary>
+        /// <returns>Returns <see cref="bool.TrueString"/> if you're successufy attached.</returns>
+        bool AttachProcess();
+
+        /// <summary>
+        /// Disconnect your tool from your ps3.
+        /// </summary>
+        /// Return 0 if you are disconnected.
+        int DisconnectTarget();
+
+        /// <summary>
+        /// Detach your tool FROM you game.
+        /// </summary>
+        void DetachProcess();
+
+        /// <summary>
+        /// Get byte array and save the result in your variable <paramref name="address"/>.
+        /// </summary>
+        /// <param name="address">Start offset for the search.</param>
+        /// <param name="buffer">Byte array to save search result.</param>
+        void GetMemory(uint address, byte[] buffer);
+
+        /// <summary>
+        /// Set byte array.
+        /// </summary>
+        /// <param name="address">Start offset for sending byte array.</param>
+        /// <param name="buffer">Byte array to send.</param>
+        void SetMemory(uint address, byte[] buffer);
+
+        /// <summary>
+        /// Get byte array
+        /// </summary>
+        /// <param name="address">Start offset for the search.</param>
+        /// <param name="length">Length of a byte array to get.</param>
+        /// <returns>Returns a byte array with the bytes obtained during the search.</returns>
+        byte[] GetBytes(uint address, uint length);
+    }
+}
---
+++ IgrisLib/Apis/IConnectAPI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace IgrisLib
+{
+    /// <summary>
+    /// Interface for APIs that use the ccapi console list.
+    /// </summary>
+    public interface IConnectAPI
+    {
+        /// <summary>
+        /// Connect to your ps3 with the internal ip address of the ps3.
+        /// </summary>
+        /// <param name="ip">The ip address of your ps3.</param>
+        /// <returns>Returns <see cref="bool.TrueString"/> if you're successufy connected.</returns>
+        bool ConnectTarget(string ip);
+
+        /// <summary>
+        /// Get the list of all consoles
+        /// </summary>
+        /// <returns>Return all consoles for your connection.</returns>
+        List<CCAPI.ConsoleInfo> GetConsoleList();
+    }
+}
